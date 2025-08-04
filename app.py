5a70eae47ab42c14a55f63b5908d09eccc2cd693
ahdriam <146718765+ahdriam@users.noreply.github.com>
2025-08-04 23:35:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ conn: SupabaseConnection = st.connection("supabase", type=SupabaseConnection)
 # Use the .client attribute to get the underlying Supabase client
 supabase = conn.client
 
-# Now use the Supabase client to query your table
-response = supabase.table("CRONOT").select("*").execute()
+  # -----query your table
+  # response = supabase.table("CRONOT").select("*").execute()
+  # data = response.data
+  
+  # # Display the results
+  # st.write(response.data)
 
-# Display the results
-st.write(response.data)
-
+options = ["ריח רע", "גרפיטי", "ריפוד קרוע", "לכלוך אחר בקרון חיצוני", "נזק בצבע הקרון"]
+selected = st.multiselect(
+    ":בחר תכונה",
+    options,
+    max_selections=1
+)
 
