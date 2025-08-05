1ccb2a591fea3002a5a901ecf00c54a6bece3025
ahdriam <146718765+ahdriam@users.noreply.github.com>
2025-08-06 01:14:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,11 @@ try:
 except Exception as e:
     st.error(f"שגיאה: {e}")
 
+if enable_refresh:
+    # Wait a bit before rerunning
+    time.sleep(refresh_interval)
+    st.experimental_rerun()
+
 
 
 
