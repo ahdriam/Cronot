f642662b0cb058293ff2fb86b8c2dc3ff731fab8
ahdriam <146718765+ahdriam@users.noreply.github.com>
2025-08-06 00:06:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from st_supabase_connection import SupabaseConnection
-
+conn = st.connection("supabase", type=SupabaseConnection)
 
 
 options = ["ריח רע", "גרפיטי", "ריפוד קרוע", "לכלוך אחר בקרון חיצוני", "נזק בצבע הקרון"]
@@ -10,14 +10,18 @@ st.write("You selected:", selected_option)
 
 if selected_option:
     try:
-        response = supabase.table("CRONOT").select(selected_option).execute()
-        column_data = [row[selected_option] for row in response.data]
+        # Use query() to fetch the column
+        rows = conn.query(f"SELECT \"{selected_option}\" FROM CRONOT").execute()
+
+        # Extract the values from the rows
+        column_data = [row[selected_option] for row in rows.data]
 
         st.write(f"נתונים לעמודה '{selected_option}':")
         st.write(column_data)
 
     except Exception as e:
         st.error(f"שגיאה: לא ניתן לשלוף את העמודה '{selected_option}' — {str(e)}")
+
 
 
 
