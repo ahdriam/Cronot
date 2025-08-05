4c9bf5e3ab90a01f8af51887837e02ed1a1ffcb2
ahdriam <146718765+ahdriam@users.noreply.github.com>
2025-08-05 23:57:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ from st_supabase_connection import SupabaseConnection
 
 
 options = ["ריח רע", "גרפיטי", "ריפוד קרוע", "לכלוך אחר בקרון חיצוני", "נזק בצבע הקרון"]
-selected = st.multiselect(
-    ":בחר תכונה",
-    options,
-    max_selections=1
-)
-selected_option = selected[0] if selected else None
+
+selected_option = st.selectbox(":בחר תכונה", options)
+
 st.write("You selected:", selected_option)
+
+
 
 
 
